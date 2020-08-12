6ba5e50566a0255337df60f4b0ba76e371270ff4
Mitchell Lee <development@zyrn.dev>
2020-08-12 15:42:37 +1000

    Added document meta data

--- pages/profile.js
+++ pages/profile.js
@@ -2,11 +2,17 @@ import Layout from '../components/layout'
 import { useFetchUser } from '../lib/user'
 import Profile from '../components/profile'
 
+const meta = {
+  title: "Profile",
+  description: "Your profile page, a place to check and change your account details",
+  //image: "/images/pfp.webp", Mabye change this to their profile picture?
+};
+
 function Home() {
   const { user, loading } = useFetchUser();
 
   return (
-    <Layout user={user} loading={loading}>
+    <Layout user={user} loading={loading} meta={meta}>
       <h1>Next.js and Auth0 Example</h1>
 
       {loading && <p>Loading login info...</p>}
