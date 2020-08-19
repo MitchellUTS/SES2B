8eb7fed5fff3300509f444320e6b3ebfeb151840
Renzeu <renzeu.sin@gmail.com>
2020-08-19 16:35:42 +1000

    Changes Form done

--- components/create.js
+++ components/create.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import styles from './modify.module.css';
 
-class Modify extends Component {
+class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
