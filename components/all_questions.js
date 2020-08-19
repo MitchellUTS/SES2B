3c569efe179f67a170de860d976e2d6d1d4ec63e
Renzeu <renzeu.sin@gmail.com>
2020-08-19 15:10:08 +1000

    fixed wrapping, added thead and tbody

--- components/all_questions.js
+++ components/all_questions.js
@@ -6,37 +6,40 @@ function All_questions()
 
     return(
 <table className={styles.table}>
-  <tr>
-    <th>Questions</th>
-    <th>Action</th>
-    
-  </tr>
-  <tr>
-    <td>Question Question Question</td>
-    <td><button type="button">Detail</button>
-    <button type="button">Delete</button></td>
-  </tr>
-  <tr>
-    <td>Question Question Question</td>
-    <td><button type="button">Detail</button>
-    <button type="button">Delete</button></td>
-  </tr>
-  <tr>
-    <td>Question Question Question</td>
-    <td><button type="button">Detail</button>
-    <button type="button">Delete</button></td>
-  </tr>
-  <tr>
-    <td>Question Question Question</td>
-    <td><button type="button">Detail</button>
-    <button type="button">Delete</button></td>
-  </tr>
-  <tr>
-    <td>Question Question Question</td>
-    <td><button type="button">Detail</button>
-    <button type="button">Delete</button></td>
-  </tr>
- 
+  <thead>
+    <tr>
+      <th>Questions</th>
+      <th>Action</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Question Question Question</td>
+      <td><button type="button">Detail</button>
+      <button type="button">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Question Question Question</td>
+      <td><button type="button">Detail</button>
+      <button type="button">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Question Question Question</td>
+      <td><button type="button">Detail</button>
+      <button type="button">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Question Question Question</td>
+      <td><button type="button">Detail</button>
+      <button type="button">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Question Question Question</td>
+      <td><button type="button">Detail</button>
+      <button type="button">Delete</button></td>
+    </tr>
+  </tbody>
 </table>
     
     );
