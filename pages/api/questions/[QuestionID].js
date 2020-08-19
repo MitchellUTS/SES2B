78ccd4f63e5c1cf408ca738d854aac220eee6b76
Mitchell Lee <development@zyrn.dev>
2020-08-19 12:45:34 +1000

    Added data to the stub endpoints

--- pages/api/questions/[QuestionID].js
+++ pages/api/questions/[QuestionID].js
@@ -1,24 +1,50 @@
 // import { Question } from '../../../lib/database';
 
+const dummyQuestions = [
+    {
+        id: "1",
+        question: "Evaluate the following expression: 1 + 1 = ?",
+        answer: "2",
+        level: 1,
+    },
+    {
+        id: "2",
+        question: "Evaluate the following expression: 3 + 2 = ?",
+        answer: "5",
+        level: 1,
+    },
+    {
+        id: "3",
+        question: "Evaluate the following expression: 2 - 1 = ?",
+        answer: "1",
+        level: 2,
+    },
+];
+
 export default async function get(req, res) {
     try {
-        let question_id = req.query.QuestionID;
+        let question_id = parseInt(req.query.QuestionID)-1;
+        let question = dummyQuestions[question_id];
+        if (dummyQuestions[question_id] == undefined) {
+            res.status(404).json({ error: { code: 404, message: "Resource Not Found" } });
+            return; 
+        }
 
         switch(req.method) {
             case "GET":
-                res.status(200).end("Found");
-              break;
+                    res.status(200).json(question);
+                break;
             case "PUT":
-              res.status(200).end("Updated");
-              break;
+                    res.status(200).json(question);
+                break;
             case "DELETE":
-                res.status(200).end("Deleted");
-              break;
+                    res.status(204).end();
+                break;
             default:
-                res.status(405).end("Method Not Supported");
+                res.status(405).json({ error: { code: 405, message: "Method Not Supported" } });
           }
           
     } catch (err) {
-        res.status(500).end("Internal Server Error: " + err);
+      res.status(500).json({ error: { code: 500, message: "Internal Server Error: " + err } });
     }
 }
--- pages/api/questions/index.js
+++ pages/api/questions/index.js
@@ -1,19 +1,40 @@
- import { Question } from '../../../lib/database';
+// import { Question } from '../../../lib/database';
+
+const dummyQuestions = [
+    {
+        id: "1",
+        question: "Evaluate the following expression: 1 + 1 = ?",
+        answer: "2",
+        level: 1,
+    },
+    {
+        id: "2",
+        question: "Evaluate the following expression: 3 + 2 = ?",
+        answer: "5",
+        level: 1,
+    },
+    {
+        id: "3",
+        question: "Evaluate the following expression: 2 - 1 = ?",
+        answer: "1",
+        level: 2,
+    },
+];
 
 export default async function get(req, res) {
     try {    
         switch(req.method) {
             case "GET":
-                res.status(200).end("Found");
+                res.status(200).json(dummyQuestions);
                 break;
             case "POST":
-                res.status(201).end("Created");
+                res.status(201).json(dummyQuestions[0]);
                 break;
             default:
-                res.status(405).end("Method Not Supported");
+                res.status(405).json({ error: { code: 405, message: "Method Not Supported" } });
             }
             
     } catch (err) {
-        res.status(500).end("Internal Server Error: " + err);
+        res.status(500).json({ error: { code: 500, message: "Internal Server Error: " + err } });
     }
 }
