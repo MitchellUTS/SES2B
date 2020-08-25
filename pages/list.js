f306f7e74c7920caf6997555a79283d1d3ff750b
Renzeu <renzeu.sin@gmail.com>
2020-08-26 02:17:39 +1000

    Added meta to the Question-List

--- pages/list.js
+++ pages/list.js
@@ -2,14 +2,19 @@ import Layout from '../components/layout'
 import { useFetchUser } from '../lib/user'
 import Qlist from '../components/all_questions'
 
+const meta = {
+  title: "Question List",
+  description: "A list of questions that Admin can view and modify",
+  //image: "/images/pfp.webp",
+};
+
 function Home() {
   const { user, loading } = useFetchUser()
 
   return (
-    <Layout user={user} loading={loading}>
+    <Layout user={user} loading={loading} meta={meta}>
       <h1>List of all questions</h1>
-        <Qlist />
-      
+        <Qlist/>
     </Layout>
   )
 }
