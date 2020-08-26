3d8095df7966a48eeb4a21ec47d2c062ffb6120d
Ian36 <ianhuang7991@gmail.com>
2020-08-26 12:01:25 +1000

    questions api works

--- pages/api/questions/index.js
+++ pages/api/questions/index.js
@@ -1,40 +1,33 @@
 // import { Question } from '../../../lib/database';
-
-let dummyQuestions = [
-    {
-        id: "1",
-        question: "Evaluate the following expression: 1 + 1 = ?",
-        answer: "2",
-        level: 1,
-    },
-    {
-        id: "2",
-        question: "Evaluate the following expression: 3 + 2 = ?",
-        answer: "5",
-        level: 1,
-    },
-    {
-        id: "3",
-        question: "Evaluate the following expression: 2 - 1 = ?",
-        answer: "1",
-        level: 2,
-    },
-];
+const db = global.database; // Bring the database into local file scoping
 
 export default async function get(req, res) {
-    try {    
-        switch(req.method) {
+    try {
+        switch (req.method) {
             case "GET":
-                res.status(200).json(dummyQuestions);
+                let questions = await db.Question.find({});
+                res.status(200).json(questions);
                 break;
             case "POST":
-                dummyQuestions.push(req.body);
-                res.status(201).json(dummyQuestions);
+                let question = new db.Question(req.body);
+                question = await question.save();
+                res.status(201).json(question);
+                break;
+            case "DELETE":
+                const removedQuestion = await db.Question.deleteMany(
+                    {
+                        _id: req.body._id,
+                        question: req.body.question,
+                        answer: req.body.answer,
+                        level: req.body.level
+                    }
+                );
+                res.status(204).end();
                 break;
             default:
                 res.status(405).json({ error: { code: 405, message: "Method Not Supported" } });
-            }
-            
+        }
+
     } catch (err) {
         res.status(500).json({ error: { code: 500, message: "Internal Server Error: " + err } });
     }
