9fb3f38174028df1d14cf0589678bd34d7e75ab8
Ian36 <ianhuang7991@gmail.com>
2020-09-02 10:58:40 +1000

    new test table

---
+++ pages/api/tests/[TestID].js
@@ -0,0 +1,44 @@
+const db = global.database; // Bring the database into local file scoping
+
+export default async function get(req, res) {
+    try {
+        switch (req.method) {
+            case "GET":
+                let test = await db.Test.findOne(
+                    {
+                        _id: req.query.TestID
+                    }
+                );
+                res.status(200).json(test);
+                break;
+            case "PUT":
+                const updatedTest = await db.Test.updateOne(
+                    {
+                        _id: req.query.TestID
+                    },
+                    {
+                        $set:
+                        {
+                            name: req.body.name,
+                            questions: req.body.questions
+                        }
+                    }
+                );
+                res.status(200).json(updatedTest);
+                break;
+            case "DELETE":
+                await db.Test.deleteMany(
+                    {
+                        _id: req.query.TestID
+                    }
+                );
+                res.status(204).end();
+                break;
+            default:
+                res.status(405).json({ error: { code: 405, message: "Method Not Supported" } });
+        }
+
+    } catch (err) {
+        res.status(500).json({ error: { code: 500, message: "Internal Server Error: " + err } });
+    }
+}
---
+++ pages/api/tests/index.js
@@ -0,0 +1,32 @@
+const db = global.database; // Bring the database into local file scoping
+
+export default async function get(req, res) {
+    try {
+        switch (req.method) {
+            case "GET":
+                let tests = await db.Test.find({});
+                res.status(200).json(tests);
+                break;
+            case "POST":
+                let test = new db.Test(req.body);
+                test = await test.save();
+                res.status(201).json(test);
+                break;
+            case "DELETE":
+                const removedTest = await db.Test.deleteMany(
+                    {
+                        _id: req.body._id,
+                        name: req.body.name,
+                        questions: req.body.questions
+                    }
+                );
+                res.status(204).end();
+                break;
+            default:
+                res.status(405).json({ error: { code: 405, message: "Method Not Supported" } });
+        }
+
+    } catch (err) {
+        res.status(500).json({ error: { code: 500, message: "Internal Server Error: " + err } });
+    }
+}
