6a6eb104832e5de6a2e8bfc606497e92399c3dd6
Mitchell Lee <development@zyrn.dev>
2020-09-02 14:12:51 +1000

    Added some API auth support

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,41 +1,17 @@
-import auth0 from '../../lib/auth0';
-import db from '../../../lib/database';
+import authenicate from '../../lib/auth';
+import db from '../../lib/database';
 
-async function createUser(req, res, sub) {
-    // Create a new user for the current user
-    let user = new db.User({sub: sub});
-    // Save the user
-    let newUserObject = await user.save();
-    res.json(newUserObject);
-}
-
-async function deleteAllUsers(req, res) {
-    await db.User.deleteMany({});
-}
-
-async function getCurrentUser(req, res, sub) {
-    res.json(await db.User.findOne({sub: sub}));
-}
-
-export default async function users(req, res) {
+async function users(req, res, user) {
     try {
-        const session = await auth0.getSession(req)
-
-        if (!session || !session.user) {
-            res.writeHead(302, { Location: '/api/auth/login', });
-            res.end();
-            return;
-        }
-
-        // createUser(req, res, session.user.sub);
-        // deleteAllUsers(req, res);
-        getCurrentUser(req, res, session.user.sub);
-
-        // await db.User.deleteMany({});
-
+        let dbUser = await db.User.findOne({sub: sub});
+        res.json(dbUser);
     } catch (err) {
         res.status(500).end("Internal Server Error: " + err);
         throw err;
     }
+}
+
+export default function main(req, res) {
+    authenicate(req, res, users);
 }
 
