fc73c1463b5a7ed2c1de0ee946bf57ea8b4f4cd9
harrisontruong1 <h.vstruong@gmail.com>
2020-10-06 20:08:12 +1100

    Small changes

--- components/forms/createTest.js
+++ components/forms/createTest.js
@@ -2,28 +2,46 @@ import { Component } from 'react';
 import styles from './modify.module.css';
 const axios = require('axios').default;
 
-
 class CreateTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this); 
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
     render() {
         return (
-            <div className="CreateTest">
-                <h3>Test Title:</h3>
-                <textarea>
-                    Test number 1
-                </textarea>
-
+            <div className={"CreateTest", styles.container}>
+                <form>
+                    <h3>
+                        Test ID:
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </h3>
+                </form>
+                <form>
+                    <h3>
+                        Test Name:
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </h3>
+                </form>
+            
                 <h3>Test Subject:</h3>
-                <textarea>
-                    Test subject 1
-                </textarea>
+                <select>
+                    <option selected value> Arithmetic </option>
+                    <option> Algebra </option>
+                    <option> Geometry </option> 
+                    <option> Trigonometry </option>
+                </select>
 
                 <h3>Test Description:</h3>
                 <textarea>
                     this test will have x number questions 
                 </textarea>
 
-            <div>
-            <button>SUBMIT</button>
+                <div>
+                <button>SUBMIT</button>
                 </div> 
             
             </div>
